6c3a0b65d4ee49f88e0796a96c21f39e99564971
SHASHANK SINGH <singhprashantno1@gmail.com>
2020-08-01 12:11:28 +0530

    hfhd

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,25 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyB_aJ7HDIZ_pm4OhzMQ6RznsNZp4JRJ0cQ",
+    authDomain: "crwn-db-df858.firebaseapp.com",
+    databaseURL: "https://crwn-db-df858.firebaseio.com",
+    projectId: "crwn-db-df858",
+    storageBucket: "crwn-db-df858.appspot.com",
+    messagingSenderId: "666545173834",
+    appId: "1:666545173834:web:62ab04787ca0de00143db3",
+    measurementId: "G-8G1DDCSZSY"
+  };
+
+  firebase.initializeApp(config);
+
+  export const auth = firebase.auth();
+  export const firestore = firebase.firestore();
+
+  const provider = new firebase.auth.GoogleAuthProvider();
+  provider.setCustomParameters({prompt: 'select_account'});
+  export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+  export default firebase;
